990039f451a8ce707c4a2097f0b52f519ae6202c
SandraRusu17 <sandra.rusu17@gmail.com>
2021-04-20 14:03:58 +0300

    HackerRank problems and their solutions.

---
+++ src/org/hackerrank/Solution4.java
@@ -0,0 +1,43 @@
+package org.hackerrank;
+
+import java.util.*;
+import java.io.*;
+
+class Solution4 {
+    public static void main(String[] argh) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                processNumber(x);
+            } catch (Exception e) {
+                System.out.println(sc.next() + "can’t be fitted anywhere.");
+            }
+        }
+    }
+
+    private static void processNumber(long x) {
+        if (fitsInByte(x)) System.out.println("* byte");
+        if (fitsInShort(x)) System.out.println("* short");
+        if (fitsInInteger(x)) System.out.println("* int");
+        if (fitsInLong(x)) System.out.println("* long");
+    }
+
+    private static boolean fitsInLong(long x) {
+        return x >= Long.MIN_VALUE && x <= Long.MAX_VALUE;
+    }
+
+    private static boolean fitsInInteger(long x) {
+        return x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE;
+    }
+
+    private static boolean fitsInShort(long x) {
+        return x >= Short.MIN_VALUE && x <= Short.MAX_VALUE;
+    }
+
+    private static boolean fitsInByte(long x) {
+        return x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE;
+    }
+}
